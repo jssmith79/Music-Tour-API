94cee1f41b56b36da39c7a0c475bda5d7368c8a0
jssmith79 <i.am.hef@gmail.com>
2023-05-13 12:45:05 -0700

    added events and stage controllers

---
+++ controllers/event_controller.js
@@ -0,0 +1,78 @@
+const event = require('express').Router();
+const db = require('../models')
+const { Event } = db
+const { Op } = require('sequelize')
+
+//find all events
+
+event.get('/', async (req, res) => {
+    try {
+        const foundEvents = await Event.findAll({
+            order: [['available_start_time', 'ASC']],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name: ''}%`}
+            }
+        })
+        res.status(200).json(foundEvents)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//find any event
+event.get('/:id', async (req, res) => {
+    try {
+        const foundEvents = await Event.findOne({
+            where: { event_id: req.params.id }
+        })
+        res.status(200).json(foundEvents);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+event.post('/', async (req, res) => {
+    try {
+        const newEvent = await Event.create(req.body);
+        res.status(200).json({
+            mesage: 'Successfully inserted a new event.',
+            data: newEvent
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+event.put('/:id', async (req, res) => {
+    try {
+        const updatedEvent = await Event.update(req.body, {
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            messsage: `Successfully updated ${updatedEvent} events`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+
+    }
+})
+
+//delete a event
+
+event.delete('/:id', async (req, res) => {
+    try {
+        const deletedEvent = await Event.destroy({
+            where: {
+                event_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `successfully deleted ${deletedEvent} event`
+        })
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+module.exports = event;
---
+++ controllers/stage_controller.js
@@ -0,0 +1,78 @@
+const stage = require('express').Router();
+const db = require('../models')
+const { Stage } = db
+const { Op } = require('sequelize')
+
+//find all stages
+
+stage.get('/', async (req, res) => {
+    try {
+        const foundStages = await Stage.findAll({
+            order: [['available_start_time', 'ASC']],
+            where: {
+                name: { [Op.like]: `%${req.query.name ? req.query.name: ''}%`}
+            }
+        })
+        res.status(200).json(foundStages)
+    } catch (error) {
+        res.status(500).json(error)
+    }
+})
+
+//find any stage
+stage.get('/:id', async (req, res) => {
+    try {
+        const foundStages = await Stage.findOne({
+            where: { stage_id: req.params.id }
+        })
+        res.status(200).json(foundStages);
+    } catch (error) {
+        res.status(500).json(error);
+    }
+})
+
+stage.post('/', async (req, res) => {
+    try {
+        const newStage = await Stage.create(req.body);
+        res.status(200).json({
+            mesage: 'Successfully inserted a new stage.',
+            data: newStage
+        });
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+stage.put('/:id', async (req, res) => {
+    try {
+        const updatedStages = await Stage.update(req.body, {
+            where: {
+                stage_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            messsage: `Successfully updated ${updatedStages} stages`
+        })
+    } catch (err) {
+        res.status(500).json(err)
+
+    }
+})
+
+//delete a stage
+
+stage.delete('/:id', async (req, res) => {
+    try {
+        const deletedStage = await Stage.destroy({
+            where: {
+                stage_id: req.params.id
+            }
+        })
+        res.status(200).json({
+            message: `successfully deleted ${deletedStage} stage`
+        })
+    } catch(err){
+        res.status(500).json(err)
+    }
+})
+module.exports = stage;
